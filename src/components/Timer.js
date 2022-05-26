b273d2b2ba75c7a75f3b5f9c63fd13a250cf6f9b
Finn Lestrange <44287141+71xn@users.noreply.github.com>
2022-05-26 20:03:11 +0100

    v1.0 release
    This is the commit for the v1.0 release of my React Pomodoro / Study Timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,7 +1,28 @@
 import React, {useState} from "react";
 
 // UI Imports
-import {Button, Stack, createTheme, ThemeProvider, Modal, Box} from "@mui/material";
+import {
+    Button,
+    Stack,
+    createTheme,
+    ThemeProvider,
+    Modal,
+    Box,
+    Slider,
+    Grid,
+    Input,
+    Typography,
+    FormControlLabel, Checkbox
+} from "@mui/material";
+
+import {
+    Coffee,
+    CoffeeMaker,
+    NotificationsActive,
+    Settings,
+    SurroundSound,
+    Timelapse
+} from "@mui/icons-material";
 
 import {CircularProgressbar} from "react-circular-progressbar";
 import 'react-circular-progressbar/dist/styles.css';
@@ -60,14 +81,19 @@ const Timer = () => {
     const [count, setCount] = useState(0); // used to determine the next mode based on the current number of completed pomodoro
     const [totalSessions, setTotalSessions] = useState(1); // number of current pomodoro session
 
-    // Boolean to show settings modal
-    const [showSettings, setShowSettings] = useState(false);
+    const defaultTimes = [25, 5, 15];
 
     const [focusTime, setFocusTime] = useState(25);
     const [shortBreakTime, setShortBreakTime] = useState(5);
     const [longBreakTime, setLongBreakTime] = useState(10);
 
     const [running, setRunning] = useState(false); // if the timer is running or not
+
+    // Boolean to show settings modal
+    const [showSettings, setShowSettings] = useState(false);
+    const [focusTimeSettings, setFocusTimeSettings] = useState(focusTime);
+    const [shortTimeSettings, setShortTimeSettings] = useState(shortBreakTime);
+    const [longTimeSettings, setLongTimeSettings] = useState(longBreakTime);
 
     // Mode Helper Function(s) //
     // returns the time (in minutes) based on what mode the timer is in
@@ -87,11 +113,11 @@ const Timer = () => {
 
     // Calculates the timers next mode
     const nextMode = () => {
-        if (mode === "focus" & count < 3) {
+        if (mode === "focus" && count < 3) {
             setCount(prevState => prevState + 1);
             return "shortBreak";
         }
-        if (mode === "focus" & count >= 3) {
+        if (mode === "focus" && count >= 3) {
             setCount(0);
             return "longBreak";
         }
@@ -132,16 +158,25 @@ const Timer = () => {
     }
 
     // Sound Functions //
+    const [bell, setBell] = useState(true);
+    const [click, setClick] = useState(true);
+    const [bellVolume, setBellVolume] = useState(0.2);
+    const [clickVolume, setClickVolume] = useState(0.3);
+
     const playBell = () => {
-        const bell = new Audio(bellSound);
-        bell.volume = 0.2;
-        bell.play().then();
+        if (bell) {
+            const bellAudio = new Audio(bellSound);
+            bellAudio.volume = bellVolume;
+            bellAudio.play().then();
+        }
     };
 
     const playClick = () => {
-        const click = new Audio(clickSound);
-        click.volume = 0.3;
-        click.play().then();
+        if (click) {
+            const clickAudio = new Audio(clickSound);
+            clickAudio.volume = clickVolume;
+            clickAudio.play().then();
+        }
     }
 
 
@@ -167,7 +202,6 @@ const Timer = () => {
 
             resetTimer(next);
 
-            return;
         } else {
             setProgress(newTime / minutesToMs(modeTime(mode)));
             let m = getMinutes(newTime);
@@ -245,40 +279,269 @@ const Timer = () => {
         }
     }
 
+
+    // Settings Menu
+    const handleSettingsSubmit = (e) => {
+        e.preventDefault();
+        if (focusTimeSettings !== focusTime) {
+            setFocusTime(focusTimeSettings);
+        } else if (shortTimeSettings !== shortBreakTime) {
+            setShortBreakTime(shortTimeSettings);
+        } else if (longTimeSettings !== longBreakTime) {
+            setLongBreakTime(longTimeSettings);
+        }
+
+        if (mode === "focus") {
+            setMinutes(focusTimeSettings);
+            setSeconds(0);
+            setTimeLeft(minutesToMs(focusTimeSettings));
+        }
+        if (mode === "shortBreak") {
+            setMinutes(shortTimeSettings);
+            setSeconds(0);
+            setTimeLeft(minutesToMs(shortTimeSettings));
+        }
+        if (mode === "longBreak") {
+            setMinutes(longTimeSettings);
+            setSeconds(0);
+            setTimeLeft(minutesToMs(longTimeSettings));
+        }
+
+        setShowSettings(false);
+    }
+
+    const handleReset = () => {
+        // Set main state
+        setFocusTime(defaultTimes[0]);
+        setShortBreakTime(defaultTimes[1]);
+        setLongBreakTime(defaultTimes[2]);
+
+        // Set settings state
+        setFocusTimeSettings(defaultTimes[0]);
+        setShortTimeSettings(defaultTimes[1]);
+        setLongTimeSettings(defaultTimes[2]);
+
+        // Set sounds
+        setBell(true);
+        setClick(true);
+        setBellVolume(0.2);
+        setClickVolume(0.3);
+
+
+        if (mode === "focus") {
+            setMinutes(defaultTimes[0]);
+            setTimeLeft(minutesToMs(defaultTimes[0]));
+        }
+        if (mode === "shortBreak") {
+            setMinutes(defaultTimes[1]);
+            setTimeLeft(minutesToMs(defaultTimes[1]));
+        }
+        if (mode === "longBreak") {
+            setMinutes(defaultTimes[2]);
+            setTimeLeft(minutesToMs(defaultTimes[2]));
+        }
+
+        setSeconds(0);
+        setShowSettings(false);
+    }
+
+
     return (
         <>
             <ThemeProvider theme={theme}>
                 <div className={"timerContainer"}>
                     <Stack justifyContent={"center"} spacing={2} direction={"row"}>
-                        <Button color={"primary"} variant={mode === "focus" ? "contained" : "outlined"} onClick={() => {changeMode("focus"); playClick()}}>Focus
+                        <Button color={"primary"} variant={mode === "focus" ? "contained" : "outlined"} onClick={() => {
+                            changeMode("focus");
+                            playClick()
+                        }}>Focus
                             Mode</Button>
                         <Button color={"primary"} variant={mode === "shortBreak" ? "contained" : "outlined"}
-                                onClick={() => {changeMode("shortBreak"); playClick()}}>Short Break</Button>
+                                onClick={() => {
+                                    changeMode("shortBreak");
+                                    playClick()
+                                }}>Short Break</Button>
                         <Button color={"primary"} variant={mode === "longBreak" ? "contained" : "outlined"}
-                                onClick={() => {changeMode("longBreak"); playClick()}}>Long Break</Button>
+                                onClick={() => {
+                                    changeMode("longBreak");
+                                    playClick()
+                                }}>Long Break</Button>
                     </Stack>
 
-                    <div style={{height:"20%", paddingTop:"30px", paddingLeft:"30px", paddingRight:"30px", paddingBottom:"15px"}}>
-                        <CircularProgressbar styles={{text:{fill:"white", fontSize:"20px", fontFamily:"monospace"}, path:{stroke:(progress < 0.15 ? red : main), strokeLinecap:"butt"}}} value={progress} maxValue={1} text={min + ":" + sec}/>
+                    <div style={{
+                        height: "20%",
+                        paddingTop: "30px",
+                        paddingLeft: "30px",
+                        paddingRight: "30px",
+                        paddingBottom: "15px"
+                    }}>
+                        <CircularProgressbar styles={{
+                            text: {fill: "white", fontSize: "20px", fontFamily: "monospace"},
+                            path: {stroke: (progress < 0.15 ? red : main), strokeLinecap: "butt"}
+                        }} value={progress} maxValue={1} text={min + ":" + sec}/>
                     </div>
 
-                    <p style={{fontSize:"17px", fontFamily:"monospace", paddingBottom:"10px"}}>Focus Session #{totalSessions}</p>
+                    <p style={{fontSize: "17px", fontFamily: "monospace", paddingBottom: "10px"}}>Focus Session
+                        #{totalSessions}</p>
 
                     <Stack justifyContent={"center"} spacing={1} direction={"row"}>
                         {running ?
-                            <Button color={"error"} variant={"contained"} onClick={() => {stopTimer(); playClick()}}>Stop Timer</Button>
+                            <Button color={"error"} variant={"contained"} onClick={() => {
+                                stopTimer();
+                                playClick()
+                            }}>Stop Timer</Button>
                             :
-                            <Button color={"success"} variant={"contained"} onClick={() => {startTimer(); playClick()}}>Start Timer</Button>
+                            <Button color={"success"} variant={"contained"} onClick={() => {
+                                startTimer();
+                                playClick()
+                            }}>Start Timer</Button>
                         }
-                        <Button color={"error"} variant={"outlined"} onClick={() => {resetTimer(mode); playClick()}}>Reset Timer</Button>
+                        <Button color={"error"} variant={"outlined"} onClick={() => {
+                            resetTimer(mode);
+                            playClick()
+                        }}>Reset Timer</Button>
+                    </Stack>
+                    <Stack justifyContent={"center"} paddingTop={"20px"} spacing={1} direction={"row"}>
+                        <Button disabled={running} variant={"outlined"}
+                                onClick={() => setShowSettings(true)}><Settings/> Settings</Button>
                     </Stack>
                 </div>
 
-                <Button onClick={() => setShowSettings(true)}>Test</Button>
-
-                <Modal open={showSettings} onClose={() => {setShowSettings(false)}}>
+                {/* Code for the pop-up settings menu */}
+                <Modal open={showSettings} onClose={() => {
+                    setShowSettings(false);
+                    setFocusTimeSettings(focusTime);
+                    setShortTimeSettings(shortBreakTime);
+                    setLongTimeSettings(longBreakTime);
+                }}>
                     <Box sx={modalStyle}>
-                        <p>Test</p>
+                        <form onSubmit={handleSettingsSubmit}>
+                            <Box sx={{width: "250"}}>
+                                <h2>Time Settings</h2>
+                                {/* Focus Time Set */}
+                                <Typography gutterBottom>
+                                    Focus Time
+                                </Typography>
+                                <Grid container spacing={2} alignItems={"center"}>
+                                    <Grid item>
+                                        <Timelapse/>
+                                    </Grid>
+                                    <Grid item xs>
+                                        <Slider max={60} min={2} valueLabelFormat={(x) => x + " minutes"}
+                                                value={focusTimeSettings} valueLabelDisplay={"auto"}
+                                                aria-label={"Default"}
+                                                onChange={(e) => setFocusTimeSettings(e.target.value)}/>
+                                    </Grid>
+                                    <Grid item>
+                                        <Input style={{color: "white"}}
+                                               inputProps={{step: 1, min: 2, max: 60, type: 'number'}}
+                                               value={focusTimeSettings} size={"small"}
+                                               onChange={(e) => setFocusTimeSettings(parseInt(e.target.value))}/>
+                                    </Grid>
+                                </Grid>
+                                <br/>
+
+                                {/* Short Break Time Set */}
+                                <Typography gutterBottom>
+                                    Short Break Time
+                                </Typography>
+                                <Grid container spacing={2} alignItems={"center"}>
+                                    <Grid item>
+                                        <Coffee/>
+                                    </Grid>
+                                    <Grid item xs>
+                                        <Slider min={1} max={15} valueLabelFormat={(x) => x + " minutes"}
+                                                value={shortTimeSettings} valueLabelDisplay={"auto"}
+                                                aria-label={"Default"}
+                                                onChange={(e) => setShortTimeSettings(e.target.value)}/>
+                                    </Grid>
+                                    <Grid item>
+                                        <Input style={{color: "white"}}
+                                               inputProps={{step: 1, min: 1, max: 60, type: 'number'}}
+                                               value={shortTimeSettings} size={"small"}
+                                               onChange={(e) => setShortTimeSettings(parseInt(e.target.value))}/>
+                                    </Grid>
+                                </Grid>
+                                <br/>
+
+                                {/* Long Break Time Set */}
+                                <Typography gutterBottom>
+                                    Long Break Time
+                                </Typography>
+                                <Grid container spacing={2} alignItems={"center"}>
+                                    <Grid item>
+                                        <CoffeeMaker/>
+                                    </Grid>
+                                    <Grid item xs>
+                                        <Slider min={15} max={30} valueLabelFormat={(x) => x + " minutes"}
+                                                value={longTimeSettings} valueLabelDisplay={"auto"}
+                                                aria-label={"Default"}
+                                                onChange={(e) => setLongTimeSettings(e.target.value)}/>
+                                    </Grid>
+                                    <Grid item>
+                                        <Input style={{color: "white"}}
+                                               inputProps={{step: 1, min: 2, max: 60, type: 'number'}}
+                                               value={longTimeSettings} size={"small"}
+                                               onChange={(e) => setLongTimeSettings(parseInt(e.target.value))}/>
+                                    </Grid>
+                                </Grid>
+                                <br/>
+
+                                <h2>Sound Settings</h2>
+
+                                {/* Sounds */}
+                                <Typography gutterBottom>
+                                    Enable Sounds
+                                </Typography>
+                                <FormControlLabel
+                                    label={"Bell"}
+                                    control={<Checkbox inputProps={{'aria-label': 'controlled'}} checked={bell}
+                                                       onChange={(e) => setBell(e.target.checked)}/>}
+                                />
+                                <FormControlLabel
+                                    label={"Click"}
+                                    control={<Checkbox inputProps={{'aria-label': 'controlled'}} checked={click}
+                                                       onChange={(e) => setClick(e.target.checked)}/>}
+                                />
+
+                                <Typography paddingTop={"15px"} gutterBottom>
+                                    Bell Volume
+                                </Typography>
+                                <Grid container spacing={2} alignItems={"center"}>
+                                    <Grid item>
+                                        <NotificationsActive/>
+                                    </Grid>
+                                    <Grid item xs>
+                                        <Slider min={0} max={1} step={0.1} valueLabelFormat={(x) => x * 100 + "%"}
+                                                value={bellVolume} valueLabelDisplay={"auto"}
+                                                aria-label={"Default"}
+                                                onChange={(e) => {setBellVolume(e.target.value); playBell();}}/>
+                                    </Grid>
+                                </Grid>
+
+                                <Typography gutterBottom>
+                                    Click Volume
+                                </Typography>
+                                <Grid container spacing={2} alignItems={"center"}>
+                                    <Grid item>
+                                        <SurroundSound/>
+                                    </Grid>
+                                    <Grid item xs>
+                                        <Slider min={0} max={1} step={0.1} valueLabelFormat={(x) => x * 100 + "%"}
+                                                value={clickVolume} valueLabelDisplay={"auto"}
+                                                aria-label={"Default"}
+                                                onChange={(e) => {setClickVolume(e.target.value); playClick();}}/>
+                                    </Grid>
+                                </Grid>
+
+                            </Box>
+                            <br/>
+                            <Stack spacing={2} direction={"row"}>
+                                <Button color={"success"} variant={"contained"} type={"submit"}>Save Settings</Button>
+                                <Button color={"warning"} variant={"contained"} onClick={handleReset}>Reset Settings</Button>
+                                <Button color={"error"} variant={"contained"} onClick={() => setShowSettings(false)}>Exit</Button>
+                            </Stack>
+                        </form>
                     </Box>
                 </Modal>
 
