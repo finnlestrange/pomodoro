e8e259936797d7f88df92ac91dfd95278fc25863
Finn Lestrange <44287141+71xn@users.noreply.github.com>
2022-05-29 10:55:35 +0100

    cleaned up small errors

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -5,7 +5,7 @@ const Credits = () => {
     return (
       <>
           <Typography variant={"subtitle2"} paddingTop={"10px"} >
-              Created with ❤️ by <a href={"https://github.com/71xn"} target={"_blank"} style={{color:"#0b6e99"}}>Finn Lestrange</a>.
+              Created with ❤️ by <a href={"https://finnlestrange.tech"} target={"_blank"} rel={"noreferrer"} style={{color:"#0b6e99"}}>Finn Lestrange</a>. <br /> Source code: <a href={"https://github.com/71xn/pomodoro"} style={{color:"#dfab01"}} target={"_blank"} rel={"noreferrer"}>71xn/pomodoro</a>
           </Typography>
       </>
     );
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 
 // UI Imports -> https://mui.com/material-ui/
 import {
